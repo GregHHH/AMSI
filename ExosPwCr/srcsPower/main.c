b0adf56ba76d0233f5822c63198a2a08db1cdb8c
Gregory Haton <greg@mbp-de-gregory-1.home>
2019-09-22 23:02:52 +0200

    add makefile

--- ExosPwCr/srcsPower/main.c
+++ ExosPwCr/srcsPower/main.c
@@ -4,6 +4,7 @@ int	main(int argc, char **argv)
 {
 	if (argc == 3)
 		printf("%d\n", ft_iterative_power(atoi(argv[1]), atoi(argv[2])));
-	printf("%s\n", "Utilisez 2 arguments: le nombre et la puissance à laquelle il doit être mis.");
+	else
+		printf("%s\n", "Utilisez 2 arguments: le nombre et la puissance à laquelle il doit être mis.");
 	return (0);
 }
